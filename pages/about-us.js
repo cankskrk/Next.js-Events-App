8b6ce822fce398049f9bb977096db7cb1b37d0b4
cankskrk <ardacankisakurek@gmail.com>
2023-04-13 23:56:03 +0200

    Create Workspace

---
+++ pages/about-us.js
@@ -0,0 +1,13 @@
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1>ABOUT US</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        fugit culpa ipsa maxime recusandae vero est, quidem rem iste non.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUsPage;
---
+++ pages/events/index.js
@@ -0,0 +1,21 @@
+const EventsPage = () => {
+  return (
+    <div>
+      <h1>EVENTS PAGE</h1>
+      <a href="/">
+        <img />
+        <h2>Events in London</h2>
+      </a>
+      <a href="/">
+        <img />
+        <h2>Events in San Fransisco</h2>
+      </a>
+      <a href="/">
+        <img />
+        <h2>Events in Barcelona</h2>
+      </a>
+    </div>
+  );
+};
+
+export default EventsPage;
