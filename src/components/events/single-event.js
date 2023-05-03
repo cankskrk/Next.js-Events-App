f16992121b932752bb1e4fc7517a7bb2838a5f3d
cankskrk <ardacankisakurek@gmail.com>
2023-05-03 19:57:55 +0200

    feat: email registration

--- src/components/events/single-event.js
+++ src/components/events/single-event.js
@@ -1,9 +1,41 @@
-import React from "react";
-import Link from "next/link";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const SingleEvent = ({ data }) => {
-  const onSubmit = () => {};
+  const inputEmail = useRef();
+  const router = useRouter();
+  const [message, setMessage] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const emailValue = inputEmail.current.value;
+    const eventId = router?.query.id;
+
+    const validRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (!emailValue.match(validRegex)) {
+      setMessage("Please introduce a correct email address");
+    }
+
+    try {
+      const response = await fetch("/api/email_registration", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: emailValue, eventId }),
+      });
+
+      if (!response.ok) throw new Error(`${response.status}`);
+      const data = await response.json();
+      setMessage(data.message);
+      inputEmail.current.value = "";
+    } catch (e) {
+      console.log("ERROR: ", e);
+    }
+  };
 
   return (
     <div className="event_single_page">
@@ -13,12 +45,13 @@ const SingleEvent = ({ data }) => {
       <form onSubmit={onSubmit} action="" className="email_registration">
         <label>Get Registered for this event</label>
         <input
-          type="email"
+          ref={inputEmail}
           id="email"
           placeholder="Please insert your email here"
         />
-        <button type="button">Submit</button>
+        <button type="submit">Submit</button>
       </form>
+      <p>{message}</p>
     </div>
   );
 };
